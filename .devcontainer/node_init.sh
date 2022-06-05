d71d8d967ca3f3d5e9c429c60c9bff2278d3340f
avtar-mindgrep <95289348+avtar-mindgrep@users.noreply.github.com>
2022-06-05 22:05:37 +0530

    update symlink creation and removal commands

--- .devcontainer/node_init.sh
+++ .devcontainer/node_init.sh
@@ -3,17 +3,17 @@
 
 if [ -L "/workspace/development/app/gs_service" ]
 then
-     link=$(readlink /workspace/development/app/gs_service)
+     sudo link=$(readlink /workspace/development/app/gs_service)
      if ! [ $link ==  "/workspace/development/gs_service"  ]
      then
-        unlink /workspace/development/app/gs_service
-        ln -s /workspace/development/gs_service /workspace/development/app
+       sudo  unlink /workspace/development/app/gs_service
+       sudo ln -s /workspace/development/gs_service /workspace/development/app
      fi
 elif [ -d "/workspace/development/app/gs_service" ]  
 then
-    rm -rf /workspace/development/app/gs_service
-    ln -s /workspace/development/gs_service /workspace/development/app
+    sudo rm -rf /workspace/development/app/gs_service
+    sudo ln -s /workspace/development/gs_service /workspace/development/app
 
 else 
-    ln -s /workspace/development/gs_service /workspace/development/app
+    sudo ln -s /workspace/development/gs_service /workspace/development/app
 fi
