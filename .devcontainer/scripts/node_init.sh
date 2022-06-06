b99bdb3e980811bde4f294a0d1702629f4139a29
Gurjot Kaur <103424952+gurjotmindgrep@users.noreply.github.com>
2022-06-06 14:31:49 +0530

    change permissions done

--- .devcontainer/scripts/node_init.sh
+++ .devcontainer/scripts/node_init.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-cd /workspace/development/app
-npm install @mindgrep/godspeed
+#cd /workspace/development/app
+sudo npm install -g @mindgrep/godspeed
+sudo chown -R node:node /workspace/development/gs_service
 
 if [ -L "/workspace/development/app/gs_service" ]
 then
      link=$(readlink /workspace/development/app/gs_service)
      if ! [ $link ==  "/workspace/development/gs_service"  ]
      then
-       sudo  unlink /workspace/development/app/gs_service
-       sudo ln -s /workspace/development/gs_service /workspace/development/app
+       sudo unlink /workspace/development/app/gs_service
+       ln -s /workspace/development/gs_service /workspace/development/app
      fi
 elif [ -d "/workspace/development/app/gs_service" ]  
 then
     sudo rm -rf /workspace/development/app/gs_service
-    sudo ln -s /workspace/development/gs_service /workspace/development/app
+    ln -s /workspace/development/gs_service /workspace/development/app
 
 else 
-    sudo ln -s /workspace/development/gs_service /workspace/development/app
+    ln -s /workspace/development/gs_service /workspace/development/app
 fi
+
+sudo chown -R node:node /workspace/development/app/gs_service
