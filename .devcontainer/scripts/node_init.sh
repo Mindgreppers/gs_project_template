ee7ed301eabb5f9b6a038b0b93e81eff76d11c59
Sambasiva Rao Suda <samba@spinbi.com>
2022-06-07 22:03:36 +0530

    fixing docker compose and node init

--- .devcontainer/scripts/node_init.sh
+++ .devcontainer/scripts/node_init.sh
@@ -1,24 +1,4 @@
 #!/bin/bash
-
-#cd /workspace/development/app
+unlink /workspace/development/app/gs_service ||  true
+ln -s /workspace/development/gs_service /workspace/development/app
 sudo npm install -g @mindgrep/godspeed
-chown -R node:node /workspace/development/gs_service
-
-if [ -L "/workspace/development/app/gs_service" ]
-then
-     link=$(readlink /workspace/development/app/gs_service)
-     if ! [ $link ==  "/workspace/development/gs_service"  ]
-     then
-       unlink /workspace/development/app/gs_service
-       ln -s /workspace/development/gs_service /workspace/development/app
-     fi
-elif [ -d "/workspace/development/app/gs_service" ]  
-then
-    rm -rf /workspace/development/app/gs_service
-    ln -s /workspace/development/gs_service /workspace/development/app
-
-else 
-    ln -s /workspace/development/gs_service /workspace/development/app
-fi
-
-chown -R node:node /workspace/development/app/gs_service
