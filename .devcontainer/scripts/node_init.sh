5f560b57bab91f7caa812318b34b0ed3e0b5ff86
Sambasiva Rao Suda <samba@spinbi.com>
2022-06-07 11:30:45 +0530

    removing sudo

--- .devcontainer/scripts/node_init.sh
+++ .devcontainer/scripts/node_init.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 #cd /workspace/development/app
-sudo npm install -g @mindgrep/godspeed
-sudo chown -R node:node /workspace/development/gs_service
+npm install -g @mindgrep/godspeed
+chown -R node:node /workspace/development/gs_service
 
 if [ -L "/workspace/development/app/gs_service" ]
 then
      link=$(readlink /workspace/development/app/gs_service)
      if ! [ $link ==  "/workspace/development/gs_service"  ]
      then
-       sudo unlink /workspace/development/app/gs_service
+       unlink /workspace/development/app/gs_service
        ln -s /workspace/development/gs_service /workspace/development/app
      fi
 elif [ -d "/workspace/development/app/gs_service" ]  
 then
-    sudo rm -rf /workspace/development/app/gs_service
+    rm -rf /workspace/development/app/gs_service
     ln -s /workspace/development/gs_service /workspace/development/app
 
 else 
     ln -s /workspace/development/gs_service /workspace/development/app
 fi
 
-sudo chown -R node:node /workspace/development/app/gs_service
+chown -R node:node /workspace/development/app/gs_service
