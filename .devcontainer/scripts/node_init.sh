037405a32a03c1f6532d82c371dd143840c3397a
Gurjot Kaur <103424952+gurjotmindgrep@users.noreply.github.com>
2022-06-09 14:02:36 +0530

    Adding chown node:node command
    To correct issue faced when a user with UID other than 1000 try to create project on Linux.

--- .devcontainer/scripts/node_init.sh
+++ .devcontainer/scripts/node_init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo chown -R node:node /workspace/
 unlink /workspace/development/app/gs_service ||  true
 ln -s /workspace/development/gs_service /workspace/development/app
 sudo npm install -g @mindgrep/godspeed
