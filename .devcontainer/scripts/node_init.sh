4ae7eb190a02650f8534f418213cafc368426605
Sambasiva Rao Suda <samba@spinbi.com>
2022-06-10 10:57:38 +0530

    adding chown

--- .devcontainer/scripts/node_init.sh
+++ .devcontainer/scripts/node_init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo chown -R node:node /workspace/
+sudo chown -R node:node /workspace/development
 unlink /workspace/development/app/gs_service ||  true
 ln -s /workspace/development/gs_service /workspace/development/app
 sudo npm install -g @mindgrep/godspeed
